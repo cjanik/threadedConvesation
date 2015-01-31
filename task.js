e01b282d3897edb3b203d7acffa921ae61bf3a11
Chris Janik <cjanik@umich.edu>
2015-01-30 21:03:28 -0800

    done

--- task.js
+++ task.js
@@ -10,61 +10,82 @@
 	function parseConversation(){
 		$.each(topics, function(index, topic){
 
-			var element = displayTopic(topic);
-			$('#first').append(element);
+			displayTopic(topic, index);
+
 		});
 	}
 
-	function displayTopic(topic, element){
-		var element = $('<div/>').attr('class', 'comment'),
-			quoteIcon = $('<i/>').attr('class', 'quote left icon avatar'),
+	function displayTopic(topic, index){
+		var element = $('<div/>').attr('class', 'comment').attr('data-topic', index),
+			quoteIcon = $('<i/>').attr('class', 'orange quote left icon avatar'),
 			divider = $('<div/>').attr('class', 'ui section divider'),
 			topicContent = $('<div/>').attr('class', 'content'),
 			text = $('<div/>').attr('class', 'text').html(topic.topictitle);
 
 		topicContent.append(text);
-		element.append(divider, quoteIcon, topicContent);
 
+		element.append( quoteIcon, topicContent);
+
+		$('#first').append(element, divider);
 
 		if(topic.responses.length > 0){
 
-			var comments = $('<div/>').attr('class', 'comments');
+			appendResponses(topic.responses, index);
 
-		//	$.each(topic.responses, function(index, response){
-		//		console.log('Comment: ', response);
-				comments = displayResponse(topic, topic.responses[0], comments, 0);
-		//	});
-
-			// need to check comment depth and parent id here to append comments to correct content !!!!
-
-			element.append(comments);
 		}
 
-		return element;
 	}
 
-	function displayResponse(topic, response, element, index){
+
+	function appendResponses(responses, index){
+		var parentId = 0,
+			index = index.toString(),
+			comments,
+			maxParentId = Math.max.apply(null, responses.map( function(response){ return response.parentid;}) );
+
+		do {
+			
+			var commentDepth = responses.filter( function(response){
+				return response.parentid === parentId;
+			});
+
+			if( commentDepth.length > 0){
+				comments = threadResponses( commentDepth, parentId);
+
+				if(parentId === 0){
+					$("div[data-topic=" + index + "]").append( comments);
+				} else{
+					$('div[data-id=' + parentId + ']').append( comments);
+				}
+				parentId = commentDepth[0].id;
+				console.log('INDEX: ', index, 'PARENTID: ', parentId);
+			} else {
+				parentId++;
+			}
 
 
-		var responseContent = $('<div/>').attr('class', 'comment').attr('data-id', response.id).attr('data-parent-id', response.parentid).append($('<div/>').attr('class', 'content')),
-			angleIcon = $('<i/>').attr('class', 'angle double up icon avatar'),
-			author = $('<div/>').attr('class', 'author').html(response.author),
-			text = $('<div/>').attr('class', 'text').html(response.posttext),
-			date = new Date(1970,0,1);
-			date.setSeconds(Date.now() - response.age);
+		} while( parentId < maxParentId+1);
+	}
 
-		var time = $('<span/>').attr('class', 'date').html(date);
+	function threadResponses( responses, index){
 
-		responseContent.append( angleIcon, author, time, text);
+		var comments = $('<div/>').attr('class', 'comments');
 
-		if( topic.responses[index+1] && topic.responses[index+1].parentid === response.id){
-			var comments = $('<div/>').attr('class', 'comments');
-			return element.append( responseContent, displayResponse(topic, topic.responses[index+1], comments, index+1));
-		} else if( topic.responses[index+1] ){
-			return element.append( responseContent, displayResponse(topic, topic.responses[index+1], element, index+1));
-		}
+		$.each( responses, function( index, response){
+			var responseContent = $('<div/>').attr('class', 'comment').attr('data-id', response.id).attr('data-parent-id', response.parentid).append($('<div/>').attr('class', 'content')),
+				angleIcon = $('<i/>').attr('class', 'teal comment outline icon avatar'),
+				author = $('<div/>').attr('class', 'author').html(response.author),
+				text = $('<div/>').attr('class', 'text').html(response.posttext),
+				date = new Date(1970,0,1);
+				date.setSeconds(Date.now() - response.age);
 
-		return element.append(responseContent);
+			var time = $('<span/>').attr('class', 'date').html(date);
+
+			responseContent.append( angleIcon, author, time, text);
+			comments.append( responseContent);
+		});
+
+		return comments;		
 	}
 
 	
